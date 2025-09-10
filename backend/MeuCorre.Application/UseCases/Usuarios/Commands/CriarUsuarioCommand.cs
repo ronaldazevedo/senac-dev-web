e7e4f907b5109d6dd36dc584796d01375bc017f5
Ronald Azevedo <ronaldazevedosantos39@gmail.com>
2025-09-09 21:05:25 -0300

    feat: fazendo coisas

---
+++ backend/MeuCorre.Application/UseCases/Usuarios/Commands/CriarUsuarioCommand.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuCorre.Application.UseCases.Usuarios.Commands
+{
+    ///<sumary>
+    ///Comando para criar um novo usuário
+    ///Aqui você pode adicionar propriedades necessárias para criar o usuário, como Nome, Email, Senha, etc.
+    ///<sumary>
+    public class CriarUsuarioCommand : IRequest<string>
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public DateTime DataNascimento { get; set; }
+
+        public CriarUsuarioCommand(string nome, string email, string senha)
+        {
+            Nome = nome;
+            Email = email;
+            Senha = senha;
+        }   
+    }
+}
---
+++ backend/MeuCorre/Controllers/UsuarioController.cs
@@ -0,0 +1,21 @@
+﻿using MeuCorre.Application.UseCases.Usuarios.Commands;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeuCorre.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        ///<sumary>
+        ///Cria um novo usuário
+        ///<param name="command"<>/param>
+        ///<sumary>
+        [HttpPost]
+
+        public async Task<IActionResult> CriarUsuario([FromBody] CriarUsuarioCommand command)
+        {
+           
+        }
+    }
+}
