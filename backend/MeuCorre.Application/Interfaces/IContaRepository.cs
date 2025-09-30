8094279b2279b293310d1d1699e5cbeb1b7179c4
Ronald Azevedo <ronaldazevedosantos39@gmail.com>
2025-09-30 18:19:21 -0300

    feat: criei interface IContaRepository com contrato de acesso a dados

---
+++ backend/MeuCorre.Application/Interfaces/IContaRepository.cs
@@ -0,0 +1,19 @@
+﻿using MeuCorre.Domain.Entities;
+using MeuCorre.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuCorre.Application.Interfaces
+{
+    public interface IContaRepository : IRepository<Conta>
+    {
+        Task<List<Conta>> ObterPorUsuarioAsync(Guid usuarioId, bool apenasAtivas = true);
+        Task<List<Conta>> ObterPorTipoAsync(Guid usuarioId, TipoConta tipo);
+        Task<Conta?> ObterPorIdEUsuarioAsync(Guid contaId, Guid usuarioId);
+        Task<bool> ExisteContaComNomeAsync(Guid usuarioId, string nome, Guid? contaIdExcluir = null);
+        Task<decimal> CalcularSaldoTotalAsync(Guid usuarioId);
+    }
+}
