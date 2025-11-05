a4df8726bbfa88d8552cca7133e90ae769167ff9
Ronald Azevedo <ronaldazevedosantos39@gmail.com>
2025-11-04 21:52:31 -0300

    feat: ALteração em categoria

---
+++ frontend/MeuCorre/src/app/pages/categorias/categorias.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-categorias',
+  imports: [NgbNavModule],
+  templateUrl: './categorias.html',
+  styleUrl: './categorias.css',
+})
+export class Categorias {
+  active = 1;
+
+
+}
